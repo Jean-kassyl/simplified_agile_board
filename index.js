f1a4cc57a9bd5f2707d998749a63f58382ef78d4
Jean-kassyl <kassylmac@gmail.com>
2022-10-16 17:33:42 +0100

    feat: add tailwind to the project and configure it

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+const path = require('path')
 
 const app = express()
 
+app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
 app.use(express.static('public'))
@@ -12,7 +14,7 @@ app.use(bodyParser.urlencoded({extended: true}))
 
 
 app.get('/', (req, res) => {
-    res.send("<h1>Welcome where the change happen</h1>")
+    res.render('home', {title: "home page"});
 })
 
 
